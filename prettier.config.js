118af63f9cab5f9c1b7075a669e8aea94689ccff
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-31 17:40:26 +0100

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+  endOfLine: 'lf',
+  semi: false,
+  printWidth: 120,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
